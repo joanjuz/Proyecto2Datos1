ffb7b34d01d708528bf4a54d82fb724b8d335650
Felipe_Vargas13 <pipevarji@gmail.copm>
2020-11-14 19:24:19 -0600

    first branch 1.4

--- Pygame/main.py
+++ Pygame/main.py
@@ -6,14 +6,14 @@ win = pygame.display.set_mode((500, 480))
 
 pygame.display.set_caption("First Game")
 
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'),
-             pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'),
-             pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'),
-            pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'),
-            pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+walkRight = [pygame.image.load('correr_1.png'), pygame.image.load('correr_2.png'), pygame.image.load('correr_3.png'),
+             pygame.image.load('correr_4.png'), pygame.image.load('correr_5.png'), pygame.image.load('correr_6.png'),
+             pygame.image.load('correr_7.png'), pygame.image.load('correr_8.png')]
+walkLeft = [pygame.image.load('correr_1.png'), pygame.image.load('correr_2.png'), pygame.image.load('correr_3.png'),
+             pygame.image.load('correr_4.png'), pygame.image.load('correr_5.png'), pygame.image.load('correr_6.png'),
+             pygame.image.load('correr_7.png'), pygame.image.load('correr_8.png')]
 bg = pygame.image.load('bg.jpg')
-char = pygame.image.load('standing.png')
+char = pygame.image.load('caminar_0.png')
 
 clock = pygame.time.Clock()
 
