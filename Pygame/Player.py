2063d50e7f4828714e4e8209b501047520152fc0
joanjuz <62675444+joanjuz@users.noreply.github.com>
2020-12-05 02:47:26 -0600

    restructuracion del juego, para que logre funcionar lo de las plataformas

--- Pygame/Player.py
+++ Pygame/Player.py
@@ -1,133 +1,226 @@
 import pygame
-walkRight = [pygame.image.load('Recursos/Caminar/caminarD1.png'), pygame.image.load('Recursos/Caminar/caminarD2.png'),
-             pygame.image.load('Recursos/Caminar/caminarD3.png'), pygame.image.load('Recursos/Caminar/caminarD4.png')]
-walkLeft = [ pygame.image.load('Recursos/Caminar/caminarI1.png'), pygame.image.load('Recursos/Caminar/caminarI2.png'),
-             pygame.image.load('Recursos/Caminar/caminarI3.png'), pygame.image.load('Recursos/Caminar/caminarI4.png')]
+from spritesheet import Spritesheet
+from time import  sleep
+#####################################################################################
+W1 = Spritesheet('WalkR')
+walkRight = W1.get_spritte()
 
-golpeI = [pygame.image.load('Recursos/Punch/PunchI1.png'),pygame.image.load('Recursos/Punch/PunchI2.png'),pygame.image.load('Recursos/Punch/PunchI3.png'),
-          pygame.image.load('Recursos/Punch/PunchI4.png'),pygame.image.load('Recursos/Punch/PunchI5.png'),pygame.image.load('Recursos/Punch/PunchI6.png'),
-          pygame.image.load('Recursos/Punch/PunchI7.png')]
-golpeD = [pygame.image.load('Recursos/Punch/PunchD1.png'),pygame.image.load('Recursos/Punch/PunchD2.png'),pygame.image.load('Recursos/Punch/PunchD3.png'),
-          pygame.image.load('Recursos/Punch/PunchD4.png'),pygame.image.load('Recursos/Punch/PunchD5.png'),pygame.image.load('Recursos/Punch/PunchD6.png'),
-          pygame.image.load('Recursos/Punch/PunchD7.png')]
-punchI = [pygame.image.load('Recursos/Golpe/golpeI1.png'),pygame.image.load('Recursos/Golpe/golpeI2.png'),pygame.image.load('Recursos/Golpe/golpeI3.png'),
-          pygame.image.load('Recursos/Golpe/golpeI4.png'),pygame.image.load('Recursos/Golpe/golpeI5.png'),pygame.image.load('Recursos/Golpe/golpeI6.png')]
-punchD = [pygame.image.load('Recursos/Golpe/golpeD1.png'),pygame.image.load('Recursos/Golpe/golpeD2.png'),pygame.image.load('Recursos/Golpe/golpeD3.png'),
-          pygame.image.load('Recursos/Golpe/golpeD4.png'),pygame.image.load('Recursos/Golpe/golpeD5.png'),pygame.image.load('Recursos/Golpe/golpeD6.png')]
-jumpD = [pygame.image.load('Recursos/Salto/SaltoD1.png'),pygame.image.load('Recursos/Salto/SaltoD2.png'),pygame.image.load('Recursos/Salto/SaltoD3.png'),
-          pygame.image.load('Recursos/Salto/SaltoD4.png'),pygame.image.load('Recursos/Salto/SaltoD5.png'),pygame.image.load('Recursos/Salto/SaltoD6.png'),pygame.image.load('Recursos/Salto/SaltoD7.png'),
-          pygame.image.load('Recursos/Salto/SaltoD8.png'),pygame.image.load('Recursos/Salto/SaltoD9.png'),pygame.image.load('Recursos/Salto/SaltoD10.png')]
-jumpI = [pygame.image.load('Recursos/Salto/SaltoI1.png'),pygame.image.load('Recursos/Salto/SaltoI2.png'),pygame.image.load('Recursos/Salto/SaltoI3.png'),
-          pygame.image.load('Recursos/Salto/SaltoI4.png'),pygame.image.load('Recursos/Salto/SaltoI5.png'),pygame.image.load('Recursos/Salto/SaltoI6.png'),pygame.image.load('Recursos/Salto/SaltoI7.png'),
-          pygame.image.load('Recursos/Salto/SaltoI8.png'),pygame.image.load('Recursos/Salto/SaltoI9.png'),pygame.image.load('Recursos/Salto/SaltoI10.png')]
+W2 = Spritesheet('WalkL')
+walkLeft = W2.get_spritte()
 
+G1 = Spritesheet('GolpeL')
+golpeI = G1.get_spritte()
+
+G2 = Spritesheet('GolpeR')
+golpeD = G2.get_spritte()
+
+P1 = Spritesheet('punchL')
+punchI = P1.get_spritte()
+
+P2 = Spritesheet('punchR')
+punchD = P2.get_spritte()
+
+J1 = Spritesheet('jumpR')
+jumpD = J1.get_spritte()
+
+J2 = Spritesheet('jumpL')
+jumpI = J2.get_spritte()
+
+
+#############################################################################################
 class player(object):
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.vel = 10
+    def __init__(self):
+        ################################
+        self.image = walkRight[0]
+        self.rect = self.image.get_rect()
+        ###################################
         self.isJump = False
         self.isjumpcount = 0
+        self.on_ground = False
+        ###########################
+        self.gravity, self.friction = .1, -.12
+        ###############################
+        self.position = pygame.math.Vector2(0,0)
+        self.vel = pygame.math.Vector2(0, 0)
+        #################
+        self.acceleration = pygame.math.Vector2(0, self.gravity)
+        ################
+        self.standing = True
         self.left = False
         self.right = False
-        self.hit = False
         self.walkCount = 0
-        self.jumpCount = 10
-        self.standing = True
-        self.hitbox = (self.x + 17, self.y + 11, 29, 52)
+        ###############################
+        self.hit = False
+        ###############################
+        self.hitbox = (self.position.x + 17, self.position.y + 11, 29, 52)
+        ###############################
         self.punch = False
         self.punchCount = 0
         self.golpe = False
         self.golpeCount = 0
-        self.bajada = 10
+        ###############################
+
+        ###############################
+        self.LEFT_KEY = False
+        self.RIGHT_KEY = False
+        self.FACING_LEFT = False
+
+        ################
+
+
 
     def draw(self, win):
-        if self.isjumpcount + 1 >= 30:
+        if self.isjumpcount + 1 >= 100:
             self.isjumpcount = 0
-        if self.walkCount + 1 >= 9:
+        if self.walkCount + 1 >= 18:
             self.walkCount = 0
-        if self.punchCount + 1 >= 21:
+        if self.punchCount + 1 >= 42:
             self.punchCount = 0
             self.punch = False
-        if self.golpeCount + 1 >= 18:
+        if self.golpeCount + 1 >= 36:
             self.golpeCount = 0
             self.golpe = False
 
         if not (self.standing):
             if self.isJump:
                 if self.left:
-                    win.blit(jumpI[self.isjumpcount // 3], (self.x, self.y))
+                    win.blit(jumpI[self.isjumpcount // 10], (self.position.x, self.position.y))
                     self.isjumpcount += 1
                 elif self.right:
-                    win.blit(jumpD[self.isjumpcount // 3], (self.x, self.y))
+                    win.blit(jumpD[self.isjumpcount // 10], (self.position.x, self.position.y))
                     self.isjumpcount += 1
             elif self.golpe:
                 if self.left:
-                    win.blit(punchI[self.golpeCount // 3], (self.x, self.y))
+                    win.blit(punchI[self.golpeCount // 6], (self.position.x, self.position.y))
                     self.golpeCount += 1
 
                 if self.right:
-                    win.blit(punchD[self.golpeCount // 3], (self.x, self.y))
+                    win.blit(punchD[self.golpeCount // 6], (self.position.x, self.position.y))
                     self.golpeCount += 1
 
             elif self.punch:
                 if self.left:
-                    win.blit(golpeI[self.punchCount // 3], (self.x, self.y))
+                    win.blit(golpeI[self.punchCount // 6], (self.position.x, self.position.y))
                     self.punchCount += 1
-                    self.hitbox = (self.x - 10, self.y + 45, 31, 20)
+                    self.hitbox = (self.position.x - 10, self.position.y + 45, 31, 20)
                     pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
 
                 if self.right:
-                    win.blit(golpeD[self.punchCount // 3], (self.x, self.y))
+                    win.blit(golpeD[self.punchCount // 6], (self.position.x, self.position.y))
                     self.punchCount += 1
-                    self.hitbox = (self.x + 70, self.y + 45, 31, 20)
+                    self.hitbox = (self.position.x + 70, self.position.y + 45, 31, 20)
                     pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
 
             elif self.left:
-                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
+                win.blit(walkLeft[self.walkCount // 6], (self.position.x, self.position.y))
                 self.walkCount += 1
-                self.hitbox = (self.x + 17, self.y + 2, 31, 120)
+                self.hitbox = (self.position.x + 17, self.position.y + 2, 31, 120)
                 pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
             elif self.right:
-                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+                win.blit(walkRight[self.walkCount // 6], (self.position.x, self.position.y))
                 self.walkCount += 1
-                self.hitbox = (self.x + 23, self.y + 2, 31, 120)
+                self.hitbox = (self.position.x + 23, self.position.y + 2, 31, 120)
                 pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
         else:
             if self.isJump:
                 if self.left:
-                    win.blit(jumpI[self.isjumpcount // 3], (self.x, self.y))
+                    win.blit(jumpI[self.isjumpcount // 10], (self.position.x, self.position.y))
                     self.isjumpcount += 1
                 else:
-                    win.blit(jumpD[self.isjumpcount // 3], (self.x, self.y))
+                    win.blit(jumpD[self.isjumpcount // 10], (self.position.x, self.position.y))
                     self.isjumpcount += 1
             elif self.golpe:
                 if self.left:
-                    win.blit(punchI[self.golpeCount // 3], (self.x, self.y))
+                    win.blit(punchI[self.golpeCount // 6], (self.position.x, self.position.y))
                     self.golpeCount += 1
                 else:
-                    win.blit(punchD[self.golpeCount // 3], (self.x, self.y))
+                    win.blit(punchD[self.golpeCount // 6], (self.position.x, self.position.y))
                     self.golpeCount += 1
             elif self.punch:
                 if self.left:
-                    win.blit(golpeI[self.punchCount // 3], (self.x, self.y))
+                    win.blit(golpeI[self.punchCount // 6], (self.position.x, self.position.y))
                     self.punchCount += 1
-                    self.hitbox = (self.x - 10, self.y + 45, 31, 20)
+                    self.hitbox = (self.position.x - 10, self.position.y + 45, 31, 20)
                     pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
 
                 else:
-                    win.blit(golpeD[self.punchCount // 3], (self.x, self.y))
+                    win.blit(golpeD[self.punchCount // 6], (self.position.x, self.position.y))
                     self.punchCount += 1
-                    self.hitbox = (self.x + 70, self.y + 45, 31, 20)
+                    self.hitbox = (self.position.x + 70, self.position.y + 45, 31, 20)
                     pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
 
             elif self.right:
-                win.blit(walkRight[0], (self.x, self.y))
-                self.hitbox = (self.x + 23 , self.y + 2, 31, 120)
+                win.blit(walkRight[0], (self.position.x, self.position.y))
+                self.hitbox = (self.position.x + 23, self.position.y + 2, 31, 120)
                 pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
             else:
-                win.blit(walkLeft[0], (self.x, self.y))
-                self.hitbox = (self.x + 17, self.y + 2, 31, 120)
+                win.blit(walkLeft[0], (self.position.x, self.position.y))
+                self.hitbox = (self.position.x + 17, self.position.y + 2, 31, 120)
                 pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
+    def update(self,dt,players):
+        self.horizontal_movement(dt)
+        self.checkCollisionsx(players)
+        self.vertical_movement(dt)
+        self.checkCollisionsy(players)
+
+
+    def horizontal_movement(self,dt):
+        self.acceleration.x = 0
+        if self.LEFT_KEY:
+            self.acceleration.x -= .5
+        elif self.RIGHT_KEY:
+            self.acceleration.x += .5
+        self.acceleration.x += self.vel.x * self.friction
+        self.vel.x += self.acceleration.x * dt
+        self.limit_velocity(15)
+        self.position.x += self.vel.x * dt + (self.acceleration.x * .5)*(dt * dt)
+        self.rect.x = self.position.x
+    def vertical_movement(self,dt):
+        self.vel.y +=  self.acceleration.y * dt
+        if self.vel.y > 7: self.vel.y = 7
+        self.position.y += self.vel.y * dt + (self.acceleration.y * .5) *(dt * dt)
+        if self.position.y > 700:
+            self.on_ground = True
+            self.vel.y = 0
+            self.position.y = 700
+        self.rect.bottom = self.position.y
+
+    def limit_velocity(self,max_vel):
+        min(-max_vel,max(self.vel.x,max_vel))
+        if abs(self.vel.x) < .01: self.vel.x = 0
+
+    def jump(self):
+        if self.on_ground:
+            self.isJump = True
+            self.vel.y -= 8
+            self.on_ground = False
+    def get_hits(self,players):
+        hits = []
+        for player in players:
+            if self.rect.colliderect(player):
+                hits.append(player)
+        return hits
+    def checkCollisionsx(self,players):
+        collisions = self.get_hits(players)
+        for player in collisions:
+            if self.vel.x > 0:
+                self.position.x = player.rect.left - self.rect.w
+                self.rect.x = self.position.x + 100
+            elif self.vel.x < 0:
+                self.position.x = player.rect.right
+                self.rect.x = self.position.x
+    def checkCollisionsy(self,players):
+        self.rect.bottom += 1
+        collisions = self.get_hits(players)
+        for player in collisions:
+            if self.vel.y > 0:
+                self.on_ground = True
+                self.isJump = False
+                self.vel.y = 0
+                self.position.y = player.rect.top
+                self.rect.bottom = self.position.y
+            elif self.vel.y < 0:
+                self.vel.y = 0
+                self.position.y = player.rect.bottom + self.rect.h
+                self.rect.bottom = self.position.y
--- Pygame/main.py
+++ Pygame/main.py
@@ -16,54 +16,127 @@ bg = pygame.image.load('Recursos/Backgroung/bg.png')
 #pygame.mixer.music.play(-1)
 
 score = 0
+TARGET_FPS = 60
 
 def redrawGameWindow():
     win.blit(bg, (0, 0))
     text = font.render('Score: ' + str(score), 1, (0, 0, 0))
     win.blit(text, (350, 10))
-    player1.draw(win)
     player2.draw(win)
+    player1.draw(win)
     pygame.display.update()
 
 
 # mainloop
 font = pygame.font.SysFont('comicsans', 30, True)
-player1 = player(300, 700, 100, 150)
-player2 = player(700, 700, 100, 150)
-shootLoop = 0
-bullets = []
+player1 = player()
+player1.position.x = 300
+player1.position.y = 700
+player2 = player()
+player2.position.x = 600
+player2.position.y = 700
 run = True
 while run:
-    clock.tick(27)
+    dt = clock.tick(60) * .001 * TARGET_FPS
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player1.LEFT_KEY = True
+                player1.left = True
+                player1.right = False
+                player1.standing = False
+                player1.punch = False
+            elif event.key == pygame.K_d:
+                player1.RIGHT_KEY = True
+                player1.left = False
+                player1.right = True
+                player1.standing = False
+                player1.punch = False
+            elif event.key == pygame.K_w:
+                player1.jump()
+            elif event.key == pygame.K_j:
+                player2.LEFT_KEY = True
+                player2.left = True
+                player2.right = False
+                player2.standing = False
+                player2.punch = False
+            elif event.key == pygame.K_l:
+                player2.RIGHT_KEY = True
+                player2.left = False
+                player2.right = True
+                player2.standing = False
+                player2.punch = False
+            elif event.key == pygame.K_i:
+                player2.jump()
+        ############################################
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                player1.LEFT_KEY = False
+                player1.left = False
+                player1.right = False
+                player1.standing = True
+                player1.punch = False
+            elif event.key == pygame.K_d:
+                player1.RIGHT_KEY = False
+                player1.LEFT_KEY = False
+                player1.left = False
+                player1.right = False
+                player1.standing = True
+                player1.punch = False
+            elif event.key == pygame.K_w:
+                if player1.isJump:
+                    player1.vel.y *= .25
+                    player1.isJump = False
+            if event.key == pygame.K_j:
+                player2.LEFT_KEY = False
+                player2.left = False
+                player2.right = False
+                player2.standing = True
+                player2.punch = False
+            elif event.key == pygame.K_l:
+                player2.RIGHT_KEY = False
+                player2.LEFT_KEY = False
+                player2.left = False
+                player2.right = False
+                player2.standing = True
+                player2.punch = False
+            elif event.key == pygame.K_i:
+                if player2.isJump:
+                    player2.vel.y *= .25
+                    player2.isJump = False
+    '''
     if player1.hitbox[1] < player2.hitbox[1] + player2.hitbox[3] and player1.hitbox[1] + player1.hitbox[3] > player2.hitbox[1]:
         if player1.hitbox[0] + player1.hitbox[2] > player2.hitbox[0] and player1.hitbox[0] < player2.hitbox[0] + player2.hitbox[2]:
             player1.hit = True
             if not (player1.punch or player2.punch):
                 if player1.right == True:
                     if player2.right == True:
-                        player1.x -= player1.vel
-                        player2.x += player2.vel
+                        player1.position.x -= player1.vel.x
+                        player2.position.x += player2.vel.x
                     else:
-                        player2.x += player2.vel
+                        player2.position.x += player2.vel.x
 
                 else:
                     if player2.right == False:
-                        player1.x += player1.vel
-                        player2.x -= player2.vel
+                        player1.position.x += player1.vel.x
+                        player2.position.x -= player2.vel.x
                     else:
-                        player2.x -= player2.vel
+                        player2.position.x -= player2.vel.x
             else:
                 if player2.punch:
                     if player2.right:
                         player1.left = True
                         player1.right = False
                         player1.golpe = True
-                        player1.x += 20
+                        player1.position.x += 20
                     else:
                         player1.right = True
                         player1.left = False
                         player1.golpe = True
-                        player1.x -= 20
+                        player1.position.x -= 20
 
         else:
             player1.hit = False
@@ -76,63 +149,37 @@ while run:
             if not (player2.punch or player1.punch):
                 if player2.right:
                     if player1.right:
-                        player2.x += player2.vel
-                        player1.x -= player1.vel
-                    player1.x += player1.vel
+                        player2.position.x += player2.vel.x
+                        player1.position.x -= player1.vel.x
+                    player1.position.x += player1.vel.x
                 else:
                     if player1.right == False:
-                        player2.x -= player2.vel
-                        player1.x += player1.vel
+                        player2.position.x -= player2.vel.x
+                        player1.position.x += player1.vel.x
                     else:
-                        player1.x -= player1.vel
+                        player1.position.x -= player1.vel.x
             else:
                 if player1.punch:
                     if player1.right:
                         player2.left = True
                         player2.right = False
                         player2.golpe = True
-                        player2.x += 20
+                        player2.position.x += 20
                     else:
                         player2.right = True
                         player2.left = False
                         player2.golpe = True
-                        player2.x -= 20
+                        player2.position.x -= 20
 
 
         else:
             player2.hit = False
     else:
         player2.hit = False
-
-    if shootLoop > 0:
-        shootLoop += 1
-    if shootLoop > 3:
-        shootLoop = 0
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    for bullet in bullets:
-        if bullet.y - bullet.radius < player2.hitbox[1] + player2.hitbox[3] and bullet.y + bullet.radius > player2.hitbox[1]:
-            if bullet.x + bullet.radius > player2.hitbox[0] and bullet.x - bullet.radius < player2.hitbox[0] + \
-                    player2.hitbox[2]:
-                #hitSound.play()
-                player2.hit = True
-                bullets.pop(bullets.index(bullet))
-
-        if bullet.x < 1550 and bullet.x > 0:
-            bullet.x += bullet.vel
-        else:
-            bullets.pop(bullets.index(bullet))
-
-    keys = pygame.key.get_pressed()
-
-
-
-
-    if keys[pygame.K_a] and player1.x > player1.vel:
-        player1.x -= player1.vel
+        '''
+    '''
+    if keys[pygame.K_a] and player1.x > player1.vel.x:
+        player1.LEFT_KEY = True
         player1.left = True
         player1.right = False
         player1.standing = False
@@ -167,6 +214,7 @@ while run:
             player1.bajada = 10
             player1.isJump = False
             player1.jumpCount = 10
+            
     if keys[pygame.K_j] and player2.x > player2.vel:
         player2.x -= player2.vel
         player2.left = True
@@ -222,12 +270,9 @@ while run:
             player2.left = False
             player2.isjumpcount = 0
             player2.walkCount = 0
-
-
-
-
-
-
+        '''
+    player2.update(dt,[player1])
+    player1.update(dt,[player2])
     redrawGameWindow()
 
 pygame.quit()
